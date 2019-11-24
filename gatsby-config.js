93c4806aee98d9db3d026dd3871ae1205873c29a
Matt Polichette <matt.polichette@gmail.com>
2019-11-24 14:05:43 -0700

    Remove starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Christy and Matt's Wedding!`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
